7606d14876affb8d87e41d948aebba4d841c9886
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2022-03-01 06:20:47 +0000

    new update

--- utils/utils.js
+++ utils/utils.js
@@ -2,13 +2,15 @@ const axios = require('axios')
 
 
 
-const GetCheermotes= async () => {
+const GetCheermotes = async () => {
+  
+
   
   const response1 = await axios.post("https://id.twitch.tv/oauth2/token?client_id=yq64xn16dlldux6vw8qnw43esory3e&client_secret=1w4ezxcv8oycue3gtjnmr61ep3jo9i&grant_type=client_credentials")
   
   const token = response1.data.access_token
 
-  
+ 
    const response = await axios.get('https://api.twitch.tv/helix/bits/cheermotes',{
     headers: {
       "Authorization":`Bearer ${token}`,
@@ -21,12 +23,6 @@ return response.data
 
 
 }
-
-
-  
- 
-  
-  
 
 
 
